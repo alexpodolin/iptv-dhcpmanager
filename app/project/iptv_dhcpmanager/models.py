0d1d875c810e6b49f0ecd39116a54e27fc4a17f1
alex_podolin <alex.podolin@gmail.com>
2021-01-14 15:34:05 +0300

    'model_subnet_edit'

--- app/project/iptv_dhcpmanager/models.py
+++ app/project/iptv_dhcpmanager/models.py
@@ -16,8 +16,8 @@ class Subnets(models.Model):
 	ip_start = models.GenericIPAddressField(protocol='IPv4', blank=False, verbose_name='ip начальный')
 	ip_end = models.GenericIPAddressField(protocol='IPv4', blank=False, verbose_name='ip конечный')
 	dns_prefix = models.CharField(max_length=30, default='eltex.local', verbose_name='dns префикс')
-	dns_main = models.GenericIPAddressField(protocol='IPv4', default='10.22.0.3', blank=False, verbose_name='Осн. dns сервер')
-	dns_res = models.GenericIPAddressField(protocol='IPv4', blank=True, null=True, verbose_name='Резерв. dns сервер')
+	dns_main = models.GenericIPAddressField(protocol='IPv4', default='10.22.0.42', blank=False, verbose_name='Осн. dns сервер')
+	dns_res = models.GenericIPAddressField(protocol='IPv4', default='10.22.0.35', blank=True, null=True, verbose_name='Резерв. dns сервер')
 
 	def __str__(self):
 		return 'description: ' + self.description
@@ -29,7 +29,6 @@ class Hosts_Allow(models.Model):
 		verbose_name_plural = 'Доступные/Зарезервированные ip'
 
 	hostname = models.CharField(max_length=30, blank=False, verbose_name='Имя хоста')
-	#mac_addr = models.CharField(max_length=17, blank=False, verbose_name='mac адрес')
 	mac_addr = MACAddressField(max_length=17, blank=False, verbose_name='mac адрес')
 	ip_addr = models.GenericIPAddressField(protocol='IPv4', blank=False, verbose_name='ip адрес')
 	description = models.CharField(max_length=100, verbose_name='Описание')
